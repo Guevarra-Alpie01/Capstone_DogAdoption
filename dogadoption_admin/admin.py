55b24295a444e3ad5e133a9061d692e1da5f4c5e
alpieguevarra <alpieguevarra12@gmail.com>
2026-02-25 22:26:44 +0800

    refresh of migrations

--- dogadoption_admin/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import  Citation
-
-
-
-@admin.register(Citation)
-class CitationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'owner_name', 'penalty', 'date_issued')
--- dogadoption_admin/apps.py
+++ dogadoption_admin/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
-class AdminConfig(AppConfig):
+class DogadoptionAdminConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dogadoption_admin'
+
+    def ready(self):
+        import dogadoption_admin.signals
